7e1fdf6d310cf026bafcc3646c23b075908d5669
Chris Dunphy <chris@megaparsec.ca>
2022-10-23 21:04:30 -0600

    Add isbn-verifier

---
+++ isbn-verifier/src/lib.rs
@@ -0,0 +1,25 @@
+/// Determines whether the supplied string is a valid ISBN number
+pub fn is_valid_isbn(isbn: &str) -> bool {
+    if isbn.is_empty() {
+        return false;
+    }
+    let digits = isbn
+        .chars()
+        .filter_map(|ch| match ch {
+            '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => ch.to_digit(10),
+            'x' | 'X' => Some(10u32),
+            _ => None,
+        })
+        .collect::<Vec<u32>>();
+    do_isbn_checksum(&digits)
+}
+
+fn do_isbn_checksum(digits: &[u32]) -> bool {
+    let mut check_sum_mul = 10;
+    let checksum = digits.iter().fold(0, |acc, d| {
+        let res = acc + (d * check_sum_mul);
+        check_sum_mul -= 1;
+        res
+    });
+    checksum % 11 == 0
+}
